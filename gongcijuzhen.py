4852d7906087c9ebeb6e643762cab68ea89e8002
snczw <snczww@hotmail.com>
2018-03-02 19:47:12 +0800

    性能优化

--- gongcijuzhen.py
+++ gongcijuzhen.py
@@ -23,22 +23,50 @@ def log(func):
         return re
     return wrapper
 
+@log
+def get_dict(data):
+    formated_data = []
+    for ech in data:
+        ech_line = ech.split('/')
+        formated_data.append(ech_line)
+
+    originlines = []
+    dict1 = {}
+    for row in formated_data:
+        originlines.append(row)
+        for word in row:
+            if word != '':
+                if word in dict1:
+                    dict1[word] += 1
+                else:
+                    dict1[word] = 1
+    return dict1
+
 
 @log
-def get_set_key(data):
-    '''构建一个关键词集合，用于作为共现矩阵的首行和首列'''
-    all_key = '/'.join(data)
-    key_list = all_key.split('/')
-    set_key_list = list(filter(lambda x: x != '', key_list))
-    return list(set(set_key_list))
+def get_set_key(dic,threshold):
+    '''选取频数大于等于Threshold的关键词构建一个集合，用于作为共现矩阵的首行和首列'''
+    wf = {k: v for k, v in dic.items() if v >= threshold}
+    set_key_list=[]
+    for a in sorted(wf.items(), key=lambda item: item[1], reverse=True):
+        set_key_list.append(a[0])
+    return set_key_list
 
 
 @log
-def format_data(data):
+def format_data(data,set_key_list):
     '''格式化需要计算的数据，将原始数据格式转换成二维数组'''
     formated_data = []
     for ech in data:
         ech_line = ech.split('/')
+
+        temp=[]            # 筛选出format_data中属于关键词集合的词
+        for e in ech_line:
+            if e in set_key_list:
+                temp.append(e)
+        ech_line=temp
+
+        ech_line = list(set(filter(lambda x: x != '', ech_line))) #set去掉重复数据
         formated_data.append(ech_line)
     return formated_data
 
@@ -64,25 +92,32 @@ def init_matrix(set_key_list, matrix):
 @log
 def count_matrix(matrix, formated_data):
     '''计算各个关键词共现次数'''
+    keywordlist=matrix[0][1:]  #列出所有关键词
+    appeardict={}  #每个关键词与 [出现在的行(formated_data)的list] 组成的dictionary
+    for w in keywordlist:
+        appearlist=[]
+        i=0
+        for each_line in formated_data:
+            if w in each_line:
+                appearlist.append(i)
+            i +=1
+        appeardict[w]=appearlist
     for row in range(1, len(matrix)):
         # 遍历矩阵第一行，跳过下标为0的元素
         for col in range(1, len(matrix)):
                 # 遍历矩阵第一列，跳过下标为0的元素
                 # 实际上就是为了跳过matrix中下标为[0][0]的元素，因为[0][0]为空，不为关键词
-            if matrix[0][row] == matrix[col][0]:
-                # 如果取出的行关键词和取出的列关键词相同，则其对应的共现次数为0，即矩阵对角线为0
-                matrix[col][row] = str(0)
+            if col >= row:
+                #仅计算上半个矩阵
+                if matrix[0][row] == matrix[col][0]:
+                    # 如果取出的行关键词和取出的列关键词相同，则其对应的共现次数为0，即矩阵对角线为0
+                    matrix[col][row] = str(0)
+                else:
+                    counter = len(set(appeardict[matrix[0][row]])&set(appeardict[matrix[col][0]]))
+
+                    matrix[col][row] = str(counter)
             else:
-                counter = 0
-                # 初始化计数器
-                for ech in formated_data:
-                        # 遍历格式化后的原始数据，让取出的行关键词和取出的列关键词进行组合，
-                        # 再放到每条原始数据中查询
-                    if matrix[0][row] in ech and matrix[col][0] in ech:
-                        counter += 1
-                    else:
-                        continue
-                matrix[col][row] = str(counter)
+                matrix[col][row]=matrix[row][col]
     return matrix
 
 
@@ -90,8 +125,9 @@ def main():
     keyword_path = r'outt.xlsx'
     output_path = r'1.txt'
     data = readxls_col(keyword_path)[0]
-    set_key_list = get_set_key(data)
-    formated_data = format_data(data)
+    dict1=get_dict(data)
+    set_key_list = get_set_key(dict1,1)
+    formated_data = format_data(data,set_key_list)
     matrix = build_matirx(set_key_list)
     matrix = init_matrix(set_key_list, matrix)
     result_matrix = count_matrix(matrix, formated_data)
---
+++ gongxianjuzhen2.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/2 19:19
+# @Author  : zww
+# @Site    : 
+# @File    : gongxianjuzhen2.py
+# @Software: PyCharm
+
+import xlrd
+
+#读入表格数据,返回形如['/././','././','/././','/././']格式的列表
+def readxls_bycol(path,colnum):
+    xl = xlrd.open_workbook(path)
+    table = xl.sheets()[0]
+    data = list(table.col_values(colnum))
+    print(data)
+    print('----------1---------')
+    return data
+
+#处理表格数据, 返回无重复的所有出现过的关键词set
+def deal_data(data):
+    data_list = []
+    data_set = set()
+    for i in data:
+         data_list.extend(i.split('/'))
+#    data_list.sort()   #!!!高亮, 升序排列??????
+    data_set=set(data_list)
+    print(data_set)
+    print('----------2---------')
+    return data_set
+
+#根据set,可建立一个二维列表,并填充其其一行以及第一列, 返回建好框架的二维列表
+def creat_list_2d(data_set):
+    i = len(data_set)+1
+    #list1=[['' for x in range(i)] for y in range(i)]
+    list_2d = [[0 for col in range(i)] for row in range(i)]  #建一个空二维列表的方法噢~
+    n=1
+    for row_1 in data_set:
+        list_2d[0][n] = row_1   #填充第一行数据
+        n+=1
+        if n == i:
+            break
+    print(list_2d)
+    m=1
+    print(data_set)
+    for cols in data_set:    #填充第一列数据
+        list_2d[m][0] = cols
+        m += 1
+        if m == i:
+            break
+    print(list_2d)
+    print('----------3---------')
+    return list_2d
+
+
+#计算共现次数, 填充二维列表~  返回填好的列表~
+def count_data(list_2d,data,data_set):
+    data_formted= []
+    for i in data:
+        data_formted.append(i.split('/'))
+    print(data_formted)
+    print('----------4---------')
+    for row in range(1,len(data_set)):
+        for col in range(1,len(data_set)):
+            if row == col:
+                continue
+            else:
+                counter = 0
+                for i in data_formted:
+                    if list_2d[col][0] in i and list_2d[0][row] in i :
+                        counter += 1
+                list_2d[row][col] = counter
+    print(list_2d)
+    print('----------5---------')
+    return list_2d
+
+#把矩阵写进txt~~~~
+
+def putdata_intotxt(path,matrix):
+    with open(path,'w') as f :
+        for row in range(0,len(matrix)):
+            for col in range(0,len(matrix)):#二维列表中的每一个元素都走一遍
+                f.write(str(matrix[row][col]) + '\t')  #因为write()只接字符串类型啊
+            f.write('\n')
+
+def main():
+    path_xls = r'test.xlsx'     #---测试数据---
+    path_txt= r'共现矩阵.txt'    #---测试数据---
+    colnum = 0
+    data = readxls_bycol(path_xls,colnum)
+    data_set = deal_data(data)
+    list_2d = creat_list_2d(data_set)
+    matrix = count_data(list_2d,data,data_set)
+    print(matrix)
+    putdata_intotxt(path_txt,matrix)
+
+
+if __name__=='__main__':
+    main()
+    print('你的文件夹多了一个共现矩阵的结果~快去看看吧XP')
